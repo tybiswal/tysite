7f5f27ba7d498a0947e43abbb9db9c58155102ac
Tyagadipta Biswal <ty.biswal@gmail.com>
2024-03-11 22:40:32 +0630

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,17 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `tysite`,
+    title: "Tyagadipta's Gatsby Site",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
